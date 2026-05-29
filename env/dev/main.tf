d7ac6be1a2563fbbc1188ad2fcc5b276337b52df
vidya7991 <contact.vidya007@gmail.com>
2026-05-29 18:52:39 -0400

    fixed issues

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,7 +1,7 @@
 module "iam_role_dev" {
   source = "../../../modules/iam-role"
 
-  role_name = "role_dev"
+  role_name = "role_dev_v2"
 
   tags = {
     Environment = "dev"
--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,7 +1,7 @@
 module "iam_role_prod" {
   source = "../../../modules/iam-role"
 
-  role_name = "role_prod"
+  role_name = "role_prod_v2"
 
   tags = {
     Environment = "prod"
